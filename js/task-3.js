71189bafd8471a085ab99b44889b6662b88ae394
svitlarp <svitlanka.ripa@gmail.com>
2025-04-25 09:41:52 +0200

    remove draft

--- js/task-3.js
+++ js/task-3.js
@@ -31,17 +31,3 @@ builder.padEnd("^");
 console.log(builder.getValue()); // "^.^"
 builder.padBoth("=");
 console.log(builder.getValue()); // "=^.^="
-
-
-
-
-
-// let str = "Hello, World!";
-// let ch = "*";
-// let res = `${ch}${str}`;
-// console.log(res);
-
-
-let str = "Hello Sv";
-let ch = "**!"
-console.log(`${ch} ${str} ${ch}`);
